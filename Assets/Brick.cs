4aeb959044b1e258c165de1e74771a67adb69622
ruihichri <ruihichri@ecs.vuw.ac.nz>
2026-06-27 14:28:24 +1200

    Full in skeleton of Brick abstract class
    Complete the basic structure of the Brick anstract class which represents the basic structure of a Brick element.

--- Assets/Brick.cs
+++ Assets/Brick.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 
-public class Brick
+/**
+ * Abstract class representing common brick attributes for simple System bricks.
+ */
+public abstract class Brick
 {
+    public string PartId { get; } //The ID of the brick
+    public string PartName { get; } //The common part name of the brick
+    public Color PartColour { get; set; } //The colour of the brick
+
+    protected Brick(string partId, string partName, Color partColour)
+    {
+        this.PartId = partId;
+        this.PartName = partName;
+        this.PartColour = partColour;
+    }
+
+    public abstract int StudWidth { get; } //Represents the width measured in studs
+    public abstract int StudLength { get; } //Represents the length measured in studs
+    public abstract int TileHeight { get; } //Represents the height measures in tiles
     
+    public virtual string GetDescription()
+    {
+        return $"{PartColour} {PartName} ({StudWidth}x{StudLength}x{TileHeight})";
+    }
 }
+
