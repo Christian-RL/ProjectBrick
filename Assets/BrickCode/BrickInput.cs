42604205d02ba5a2703e9e883d6b836bde1f8a07
ruihichri <ruihichri@ecs.vuw.ac.nz>
2026-07-17 21:45:50 +1200

    Commented BrickInput

---
+++ Assets/BrickCode/BrickInput.cs
@@ -0,0 +1,44 @@
+using UnityEngine.InputSystem;
+
+namespace BrickCode
+{
+    /**
+     * Determines what type of input is currently activated.
+     */
+    public static class BrickInput
+    {
+        public static bool CtrlOrCommandHeld()
+        {
+            if (Keyboard.current == null) return false;
+            return
+                Keyboard.current.leftCtrlKey.isPressed ||
+                Keyboard.current.rightCtrlKey.isPressed ||
+                Keyboard.current.leftCommandKey.isPressed ||
+                Keyboard.current.rightCommandKey.isPressed;
+        }
+
+        public static bool ShiftHeld()
+        {
+            if (Keyboard.current == null) return false;
+            return
+                Keyboard.current.leftShiftKey.isPressed ||
+                Keyboard.current.rightShiftKey.isPressed;
+        }
+
+        public static bool AltOrOptionHeld()
+        {
+            if (Keyboard.current == null) return false;
+            return
+                Keyboard.current.leftAltKey.isPressed ||
+                Keyboard.current.rightAltKey.isPressed;
+        }
+
+        public static bool CameraControlInputIsActive()
+        {
+            if (Mouse.current == null) return false;
+            bool middleMouseHeld = Mouse.current.middleButton.isPressed;
+            bool rightMouseHeld = Mouse.current.rightButton.isPressed;
+            return AltOrOptionHeld() || middleMouseHeld || rightMouseHeld;
+        }
+    }
+}
